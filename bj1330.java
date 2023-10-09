55eab84485e02a696026aaded8d71c53c8fe151a
sphill98 <sphill9803@gmail.com>
2023-10-09 19:35:00 +0900

    Update bj1330.java

--- bj1330.java
+++ bj1330.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
  
- 		String str = br.readLine();
-		StringTokenizer st = new StringTokenizer(str," ");
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
+ 	String str = br.readLine();
+	StringTokenizer st = new StringTokenizer(str," ");
+	int a = Integer.parseInt(st.nextToken());
+	int b = Integer.parseInt(st.nextToken());
 		
         if (a < b){
             System.out.println("<");
